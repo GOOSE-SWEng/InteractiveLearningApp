c57539558b858653068d2f935623b68305d292b5
Tom Pound <tompound@ymail.com>
2020-02-15 20:47:19 +0000

    Initial Commit - Created window

--- src/InteractiveLearningApp.java
+++ src/InteractiveLearningApp.java
@@ -1,17 +1,28 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class InteractiveLearningApp extends Application{
-
+	private int defaultXSize = 1280;
+	private int defaultYSize = 720;
+	private Stage stage;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Running...");
+		launch(args);
+		System.out.println("Finished...");
 
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void start(Stage primaryStage) throws Exception {
+		stage = primaryStage;
+		stage.initStyle(StageStyle.TRANSPARENT);
+		stage.setWidth(defaultXSize);
+		stage.setHeight(defaultYSize);
+		Scene scene = new Scene();
+		stage.setScene(scene);
+		stage.show();	
 	}
-
 }
